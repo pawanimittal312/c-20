00b108fb45962ab2c81a0d2af1809ddb4fe60d39
pawanimittal312 <78547414+pawanimittal312@users.noreply.github.com>
2021-02-16 19:48:16 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var a,b;
+function setup() {
+  createCanvas(800,400);
+ a= createSprite(400, 200, 50, 50);
+b=createSprite(200,100,30,30);
+a.shapeColor="green"
+b.shapeColor="blue"
+
+}
+
+function draw() {
+  background(255,255,255);  
+  a.x=World.mouseX;
+  a.y=World.mouseY;
+  if(a.x-b.x<a.width/2+b.width/2 && b.x-a.x<a.width/2+b.width/2 && a.y-b.y<a.height/2+b.height/2
+    && b.y-a.y<a.height/2+b.height/2){
+      a.shapeColor="red";
+      b.shapeColor="grey";
+    }
+    else{
+      a.shapeColor="green"
+      b.shapeColor="blue"
+
+    }
+  drawSprites();
+}
